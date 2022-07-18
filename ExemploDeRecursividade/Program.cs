a109c584570f2683d103c3f4826c21a919d7a589
Josué Barros <frajolla27@gmail.com>
2022-07-18 14:11:07 -0300

    Add project files.

---
+++ ExemploDeRecursividade/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExemploDeRecursividade
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public class Error
+    {
+        private List<Error> _errors;
+
+        private string message;
+        public Error(string message)
+        {
+            this.message = message;
+        }
+
+        public void AddError(Exception e)
+        {
+            _errors.Add(new Error(e.Message));
+            if (e.InnerException == null)
+                return;
+            AddError(e.InnerException);
+        }
+    }
+}
