1a51f856e59c37d7827c64de7800991369aa575e
Josué Barros <frajolla27@gmail.com>
2022-07-19 10:29:03 -0300

    <feat>(pequeno exemplo de blockchain)

---
+++ Bloco_Blockchain/BlockChain.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bloco_Blockchain
+{
+    public class BlockChain
+    {
+        private readonly int _proofOfWorkDifficulty;
+        private readonly double _miningReward;
+
+        private List<Transaction> _pedindTrasactions;
+        public List<Block> Chain { get; set; }
+
+        public BlockChain(int proofOfWorkDifficulty, int mininReward)
+        {
+            _proofOfWorkDifficulty = proofOfWorkDifficulty;
+            _miningReward = mininReward;
+            _pedindTrasactions = new List<Transaction>();
+            Chain = new List<Block>() { CreateGenesisBlock() };
+        }
+
+        public void CreateTransaction(Transaction transaction)
+        {
+            _pedindTrasactions.Add(transaction);
+        }
+
+        public void MineBlock(string minerAddress)
+        {
+            Transaction minerRewardTransaction = new Transaction(null, minerAddress, _miningReward);
+            _pedindTrasactions.Add(minerRewardTransaction);
+
+            Block block = new Block(DateTime.Now, _pedindTrasactions);
+            block.MineBlock(_proofOfWorkDifficulty);
+
+            block.PreviousHash = Chain.Last().Hash;
+            Chain.Add(block);
+
+            _pedindTrasactions = new List();
+        }
+
+        public bool IsValidChain()
+        {
+            for(int i = 1; i < Chain.Count; i++)
+            {
+                Block previousBlock = Chain[i - 1];
+                Block currentBlock = Chain[i];
+
+                if(currentBlock.Hash != currentBlock.CreateHash())
+                    return false;
+                if(currentBlock.PreviousHash != previousBlock.Hash)
+                    return false;
+            }
+            return true;
+        }
+
+        public double GetBalance(string address)
+        {
+            double balance = 0;
+
+            foreach(Block block in Chain)
+            {
+                foreach(Transaction transaction in block.Transactions)
+                {
+                    if (transaction.From == address)
+                        balance -= transaction.Amount;
+
+                    if(transaction.To == address)
+                        balance += transaction.Amount;
+                }
+            }
+            return balance;
+        }
+
+        private Block CreateGenesisBlock()
+        {
+            List transactions = new List<Transaction> { new Transaction("", "", 0) };
+            return new Block(DateTime.Now, transactions, "0");
+        }
+    }
+}
